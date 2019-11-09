26c061df970df85aa2df2c651cb4b0fd66c3b41d
G.V.Strazds <gstrazds@hotmail.com>
2019-11-09 14:44:27 +0200

    Edit paths in scripts to work on gengo.lu.lv

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,4 +1,4 @@
-BASEDIR=/home/guntis/work/github/0_magd3
+BASEDIR=~/work
 if [ -z "$1" ]
   then
     NUMRUN="000"
@@ -14,7 +14,7 @@ fi
 export TW_RESULTS_FILE=eval_${NUMRUN}
 python $BASEDIR/MagD3/scripts/ingestion_local.py \
   --in-docker $BASEDIR/MagD3/tworld_submission_gvs01 \
-  $BASEDIR/CodaLab/${GAMESDIR}  $BASEDIR/MagD3/out/${TW_RESULTS_FILE}.json > $BASEDIR/MagD3/out/console_$NUMRUN.out
+  $BASEDIR/0_magd3/CodaLab/${GAMESDIR}  $BASEDIR/MagD3/out/${TW_RESULTS_FILE}.json > $BASEDIR/MagD3/out/console_$NUMRUN.out
 python $BASEDIR/MagD3/scripts/score.py $BASEDIR/MagD3/out/${TW_RESULTS_FILE}.json $BASEDIR/MagD3/tworld_submission_gvs01/results/
 mv $BASEDIR/MagD3/tworld_submission_gvs01/results/scores.txt $BASEDIR/MagD3/tworld_submission_gvs01/results/scores_$TW_RESULTS_FILE.txt
 
