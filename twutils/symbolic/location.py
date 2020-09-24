cf5ebe2fd8f7b9b8357e12ef1a10106202eaad19
G Strazds <gstrazds@hotmail.com>
2020-09-24 08:50:13 +0300

    Eliminating unused source file: location.py

--- twutils/symbolic/location.py
+++ twutils/symbolic/location.py
@@ -1,7 +1,7 @@
 # from typing import Optional
 # from symbolic import gv
-from .gv import logger
-from .util import text_similarity
+# from .gv import logger
+# from .util import text_similarity
 # from .game import GameInstance
 # from .event import NewEntityEvent, NewActionRecordEvent
 
