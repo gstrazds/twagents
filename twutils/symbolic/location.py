82839480b554a040c250cb425e5e96c972559223
G Strazds <gstrazds@hotmail.com>
2020-09-24 08:51:08 +0300

    Deleted unused source file location.py

--- twutils/symbolic/location.py
+++
@@ -1,8 +0,0 @@
-# from typing import Optional
-# from symbolic import gv
-# from .gv import logger
-# from .util import text_similarity
-# from .game import GameInstance
-# from .event import NewEntityEvent, NewActionRecordEvent
-
-
