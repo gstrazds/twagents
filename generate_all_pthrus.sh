b99c38eaa5e02fb5ac929a5f94013b3ceff53fc2
G.V.Strazds <gstrazds@hotmail.com>
2022-08-03 16:29:10 +0300

    Fixed a typo in generate_all_pthrus.sh

--- generate_all_pthrus.sh
+++ generate_all_pthrus.sh
@@ -11,8 +11,8 @@ python generate_playthroughs.py  gata_test --output-dir /ssd2tb/twdata/gata/play
 python generate_playthroughs.py  gata_100 --output-dir /ssd2tb/twdata/gata/playthru_data/ --do-write --overwrite
 python generate_playthroughs.py  gata_20 --output-dir /ssd2tb/twdata/gata/playthru_data/ --do-write --overwrite
 
-cp -a /ssd2dtb/twdata/gata/playthru_data/*.textds /ssd2tb/twdata/
-cp -a /ssd2dtb/twdata/ftwc/playthru_data/*.textds /ssd2tb/twdata/
+cp -a /ssd2tb/twdata/gata/playthru_data/*.textds /ssd2tb/twdata/
+cp -a /ssd2tb/twdata/ftwc/playthru_data/*.textds /ssd2tb/twdata/
 
 mv /ssd2tb/twdata/gata_100.textds /ssd2tb/twdata/gata_train.textds
 
